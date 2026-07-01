4c40033c44179f28c517f72bc0505b76022e8534
Sameer <bs.sameer1@gmail.com>
2026-07-01 15:25:50 -0700

    install ghostty from brew/pacman in install script

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -11,8 +11,16 @@ fi
 echo "=== Ghostty ==="
 
 if [ "$os" = "Darwin" ]; then
+  if ! [ -d /Applications/Ghostty.app ]; then
+    echo "  installing ghostty via brew..."
+    brew install --cask ghostty
+  fi
   link_file "$DOTFILES/ghostty/config" "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 else
+  if ! type ghostty &>/dev/null; then
+    echo "  installing ghostty via pacman..."
+    sudo pacman -S --noconfirm ghostty
+  fi
   link_file "$DOTFILES/ghostty/config" "$HOME/.config/ghostty/config"
 fi
 
